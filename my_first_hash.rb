d32834638f8d591f063015fc48d6687a35737a8c
Jazmin Matos <jmatos1993@gmail.com>
2020-08-26 21:00:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,6 @@ def my_hash
   your_hash = {"cat" => "maru", "dog" => "pluto"}
 end
 
-
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
