3f3f4fcced361b56bd960ccc662cd32220bf3475
Jazmin Matos <jmatos1993@gmail.com>
2020-08-26 21:01:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,8 +7,6 @@ def shipping_manifest
 end
 
 
-
-
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
@@ -19,6 +17,9 @@ def retrieval
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
+
+
+
 
 def adding
   shipping_manifest = {
